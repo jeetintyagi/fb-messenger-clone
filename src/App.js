b9b640a77c10558ae6710c50792ac17d90a7a352
jeetintyagi <jt9999709701@gmail.com>
2020-12-29 16:53:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    setMessages([...messages, input]);
+    setInput('');
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Messenger clone</h1>
+      <input
+        value={input}
+        placeholder={'Type your message'}
+        onChange={(event) => setInput(event.target.value)}
+      />
+      <button onClick={sendMessage}>Send</button>
+      {messages.map((message) => (
+        <h1>{message}</h1>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
