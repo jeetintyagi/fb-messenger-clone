6482756f4f8ee2af5f2b38c7c565bb977002802e
jeetintyagi <jt9999709701@gmail.com>
2021-01-04 16:34:27 +0530

    Add: styled-component

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Logo = styled.img`
+  justify-content: center;
+  align-items: center;
+  width: 50;
+  height: 50;
+  padding: 50;
+`;
+
+export default Logo;
