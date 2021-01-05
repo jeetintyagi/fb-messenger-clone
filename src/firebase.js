990e83733f0ba00ee78fe1d77fead9c6db0e2288
jeetintyagi <jt9999709701@gmail.com>
2021-01-05 10:01:56 +0530

    env variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,17 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from 'firebase';
 
+const apiKey = process.env.REACT_APP_FIREBASE_API_KEY;
+
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyCIq9xD1CWb-npUXp0GF6kFI5cTXjrlbW0',
+  apiKey: apiKey,
   authDomain: 'fb-messenger-clone-ae431.firebaseapp.com',
   databaseURL: 'https://fb-messenger-clone-ae431-default-rtdb.firebaseio.com',
   projectId: 'fb-messenger-clone-ae431',
   storageBucket: 'fb-messenger-clone-ae431.appspot.com',
   messagingSenderId: '174711075799',
   appId: '1:174711075799:web:0715b7a6fa4a7094f1d192',
-  measurementId: 'G-15SEV996F6',
+  measurementId: 'G-15SEV996F6'
 });
 
 const db = firebaseApp.firestore();
